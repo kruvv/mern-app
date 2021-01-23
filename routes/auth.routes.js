9116afd06f1d8aa2b2b4aa49366cb8bc61cdc8f5
krupkin_vv <viktor_krupkin@srisa.ru>
2021-01-23 19:54:51 +0300

    Небольшие правки ошибок

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,9 @@ const config = require('config');
 const {check, validationResult} = require('express-validator');
 const router = Router();
 
-// /api/auth/register
+/**
+ * /api/auth/register
+ */
 router.post(
     '/register',
     [
@@ -18,7 +20,7 @@ router.post(
 
         const errors = validationResult(req);
 
-        if(errors.isEmpty()) {
+        if(!errors.isEmpty()) {
            return res.status(400).json({
                errors: errors.array(),
                message: "Некорректные данные при регистрации"
@@ -44,12 +46,14 @@ router.post(
     }
 });
 
-// /api/auth/login
+/**
+ * /api/auth/login
+ */
 router.post(
     '/login',
     [
         check('email', 'Введите корректный email').normalizeEmail().isEmail(),
-        check('password', 'Введите пароль').isLength({ min: 6 }).exists()
+        check('password', 'Введите пароль').exists()
     ],
     async (req, res) => {
         try {
@@ -72,7 +76,7 @@ router.post(
             const isMatch = await bcrypt.compare(password, user.password);
 
             if(!isMatch) {
-                return res.status(400).json({ message: "Что-то не так, попробуйте снова" })
+                return res.status(400).json({ message: "Неверный пароль, попробуйте снова" })
             }
 
             const token = jwt.sign(
@@ -81,7 +85,7 @@ router.post(
                 {expiresIn: '1h'}
             );
 
-            res.json({ token, userId: user.id })
+            res.json({ token, userId: user.id });
 
         } catch (e) {
             res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
