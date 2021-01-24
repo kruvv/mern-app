e1f98cb1e65c5da1ef4be62eb20f29247375d000
krupkin_vv <viktor_krupkin@srisa.ru>
2021-01-24 15:35:02 +0300

    Добави роуты для авторизации

---
+++ client/src/pages/auth-page/AuthPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './AuthStyles.css';
+
+export const AuthPage = () => {
+    return (
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Сократи Ссылку</h1>
+                <div className="card blue darken-1">
+                <div className="card-content white-text">
+                  <span className="card-title">Авторизация</span>
+                  <div>
+
+                  </div>
+                </div>
+                <div className="card-action">
+                  <button className="btn yellow darken-4">Войти</button>
+                  <button className="btn grey lighten-1 black-text">Регистрация</button>
+                </div>
+              </div>
+            </div>
+        </div>
+    )
+}
---
+++ client/src/routes.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import { AuthPage } from './pages/auth-page/AuthPage';
+
+import { CreatePage } from './pages/create-page/CreatePage';
+import { DetailPage } from './pages/detail-page/DetailPage';
+import { LinksPage } from './pages/links-page/LinksPage';
+
+export const useRoutes = isAuthenticated => {
+    if (isAuthenticated) {
+        return (
+            <Switch>
+                <Route path="/links" exact>
+                    <LinksPage />
+                </Route>
+                <Route path="/create" exact>
+                    <CreatePage />
+                </Route>
+                <Route path="/detail/:id">
+                    <DetailPage />
+                </Route>
+                <Redirect to="/create"/>
+            </Switch>
+        )
+    }
+
+    return (
+        <Switch>
+            <Route path="/" exact>
+                <AuthPage />
+            </Route>
+            <Redirect to="/"/>
+        </Switch>
+    )
+
+}
