5679c0838b40e475288fa3b5715a58425039d115
kruvv <kruvv@mail.ru>
2021-01-24 15:35:02 +0300

    Добави роуты для авторизации

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-
+import 'materialize-css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { useRoutes } from './routes';
 
 
 function App() {
+
+  const routes = useRoutes(false);
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="container">
+        {routes}
+      </div>
+    </Router>
   );
 }
 
