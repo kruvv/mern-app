43e345c4c49f2bff3a3e359820e74e29a61888ae
kruvv <kruvv@mail.ru>
2021-02-28 18:22:09 +0300

    Добавил компонент LinksList и реализовал логику работы LinksPage

--- client/src/pages/links-page/LinksPage.js
+++ client/src/pages/links-page/LinksPage.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useState, useContext, useEffect, useCallback} from 'react';
+import { useHttp } from '../../hooks/http.hook';
+import { AuthContext } from '../../context/AuthContext';
+import { LinksList } from '../../components/links-list/LinksList';
+import { Loader } from '../../components/loader/Loader'
+
 
 export const LinksPage = () => {
+    const [links, setLinks] = useState([]);
+    const { request, loading } = useHttp();
+    const { token } = useContext(AuthContext);
+
+    const fetchLinks = useCallback(
+        async () => {
+            try {
+                const fetched = await request('/api/link', 'GET', null, {
+                    Authorization: `Bearer ${token}`
+                });
+                setLinks(fetched);
+            } catch (error) {
+
+            }
+        },
+        [token, request],
+    );
+
+    useEffect(() => {
+       fetchLinks();
+    }, [fetchLinks]);
+
+    if (loading) {
+        return <Loader />
+    }
+
     return (
-        <div>
-            <h1>Links Page</h1>
-        </div>
+        <>
+            { !loading && <LinksList links={links} /> }
+        </>
     )
 }
